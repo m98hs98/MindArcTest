7a5c11a53b4b39e880a9cdb61079e8d6c36d19ff
Mahsa <mahsahaddadi19@gmail.com>
2023-12-20 15:27:01 +1100

    changes

--- exerciseone/src/component/Card/Card.js
+++ exerciseone/src/component/Card/Card.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import './Card.css';
-import image1 from '../../assets/01.jpg';
 
 
-const Card = () => {
+
+
+const Card = (props) => {
     return (
         <div className="card custom__card">
             <div className='card__img__container'>
-                <img src={image1} className="card__img__top" alt="..." />
+                <img src={props.image} className="card__img__top" alt="..." />
                 <p className='card__footer'>
                     <small className='footer__copyright'>Powered by HTML.COM</small>
                 </p>
             </div>
             <div className="card__body">
-                <p className="card__text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                <p className="card__text">{props.text}</p>
                 <div className='card__button__container'>
                     <a href="/" className="btn btn--primary">READ MORE</a>
                 </div>
